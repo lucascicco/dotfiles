01b68c7f2eb45b4978be2829a808fe1ed0f82d24
Lucas Vitor de Cicco <lucasciccomy@gmail.com>
2022-04-19 08:29:28 -0300

    feat: load kube-ps1 to zshrc prompt

--- settings.sh
+++ settings.sh
@@ -27,8 +27,12 @@ export PATH
 # Kubernetes
 source <(kubectl completion zsh)
 command -v kubecolor >/dev/null 2>&1 && alias kubectl="kubecolor"
+
 # kube-ps1 
-[[ -s $KUBE_PS1_SH ]] && source $KUBE_PS1_SH
+if [ -s $KUBE_PS1_SH ]; then 
+  source $KUBE_PS1_SH
+  PROMPT='$(kube_ps1)'$PROMPT
+fi
 
 # Gvm
 [[ -s $GVM_SCRIPTS ]] && source $GVM_SCRIPTS
