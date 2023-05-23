05d2b85d5205c8b927a0e86ca8fbc299c28c0a1a
lucascicco <lucasvitorx1@gmail.com>
2023-05-23 09:42:51 -0300

    remove devopslibs_installer from dotfiles

--- scripts/corp/rc.sh
+++ scripts/corp/rc.sh
@@ -8,10 +8,6 @@ function disable_umbrella () {
    "$DOTFILES_SCRIPTS_DIR/umbrella.sh" -d
 }
 
-function devopslibs () {
-    python3 "$DOTFILES_SCRIPTS_DIR/python/devopslibs_installer.py" $@
-}
-
 function watch_issues() {
   cd $DEVOPS_SANTANDER_PT_REPO && watch -i 60 --clear gh issue list
 }
