c7ee49d2cf13bbe9adc27c462e18ddcafa07d7ae
lucascicco <lucasvitorx1@gmail.com>
2023-08-17 20:42:51 -0300

    refactor: rename bootstraping => bootstrapping

---
+++ scripts/bootstrapping/commons.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/dotfiles"
+LOCAL_BIN_DIR="$HOME/.local"
+LOCAL_BUILD_DIR="$HOME/.local_build"
+CONFIG_DIR="$DOTFILES_DIR/config"
+FUNCTIONS="$DOTFILES_DIR/scripts/utils/functions.sh"
+[[ -s "$FUNCTIONS" ]] && source "${FUNCTIONS}"
+CONFIG_LVIM_DIR="$HOME/.config/lvim"
+RTX_CONFIG="$HOME/.config/rtx/"
+
+# LunarVim
+LVIM_INSTALL_SCRIPT="https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh"
+EXTRA_ARGS="--no-install-dependencies"
+NVIM_SPELL_DIRS=(
+  "$HOME/.local/share/nvim/site/spell"
+  "$HOME/.local/share/lunarvim/site"
+)
+NVIM_SPELL_LANGUAGES=(
+  "en"
+  "pt"
+)
+
+PYTHON_LIBS=(
+  awscli
+  azure-cli
+  black
+  boto3
+  cmake
+  codespell
+  flake8
+  gcloud
+  haproxy
+  ipython
+  isort
+  mycli
+  mypy
+  net-tools
+  nmap
+  pgcli
+  pip
+  pipx
+  pre-commit
+  pynvim
+  ruff
+  ruff-lsp
+  tcpdump
+  tox
+  tree
+  wheel
+  whois
+  yamlfix
+  yamllint
+)
+PYTHON_INJECTIONS=(
+  "poetry poetry-plugin-up"
+  "ipython numpy pandas requests httpx"
+)
+KUBERNETES_PLUGINS=(
+  ctx
+  example
+  ns
+  popeye
+  reap
+  score
+  sniff
+  tree
+)
+GO_LIBS=(
+  github.com/controlplaneio/kubesec/v2@latest
+  github.com/hidetatz/kubecolor/cmd/kubecolor@latest
+  github.com/ipinfo/cli/ipinfo@latest
+  github.com/jesseduffield/lazydocker@latest
+  github.com/jesseduffield/lazygit@latest
+  github.com/mikefarah/yq/v4@latest
+  github.com/sachaos/tcpterm@latest
+  github.com/stern/stern@latest
+  golang.stackrox.io/kube-linter/cmd/kube-linter@latest
+  github.com/rhysd/actionlint/cmd/actionlint@latest
+  github.com/digitalocean/doctl/cmd/doctl@latest
+)
+
+mkdir -p "${LOCAL_BIN_DIR}"
+mkdir -p "${LOCAL_BUILD_DIR}"
+mkdir -p "${RTX_CONFIG}"
+
+SYMLINKS=(
+  "$CONFIG_DIR/git/gitattributes $HOME/.gitattributes"
+  "$CONFIG_DIR/git/gitconfig $HOME/.gitconfig"
+  "$CONFIG_DIR/git/gitignore $HOME/.gitignore"
+
+  "$CONFIG_DIR/rtx/config.toml $HOME/.config/rtx/config.toml"
+  "$CONFIG_DIR/rtx/node-packages $HOME/.default-nodejs-packages"
+  "$CONFIG_DIR/rtx/rust-packages $HOME/.default-cargo-crates"
+
+  "$CONFIG_DIR/zsh/zshrc $HOME/.zshrc"
+  "$CONFIG_DIR/vim/vimrc $HOME/.vimrc"
+)
+
+function _symlinks {
+  task "Update symlinks"
+  for FILE in "${SYMLINKS[@]}"; do
+    # shellcheck disable=2086
+    create_symlink ${FILE}
+  done
+}
+
+function _rtx {
+  info "installing rtx"
+  RTX_BINARY="${HOME}/.local/share/rtx/bin/rtx"
+  if [ ! -f "${RTX_BINARY}" ]; then
+    curl https://rtx.pub/install.sh | sh
+  fi
+
+  eval "$("${HOME}/.local/share/rtx/bin/rtx" activate bash)"
+  "${HOME}/.local/share/rtx/bin/rtx" self-update
+  "${HOME}/.local/share/rtx/bin/rtx" plugins update -a --install-missing
+  "${HOME}/.local/share/rtx/bin/rtx" install
+  "${HOME}/.local/share/rtx/bin/rtx" prune
+
+  mkdir -p "${HOME}/.local/share/zsh/site-functions"
+  "${HOME}/.local/share/rtx/bin/rtx" complete -s zsh >"${HOME}/.local/share/zsh/site-functions/_rtx"
+}
+
+function _zsh {
+  task "Install zsh plugins"
+  ANTIGEN_SCRIPT_PATH="$HOME/antigen.zsh"
+  if [[ ! -s "$ANTIGEN_SCRIPT_PATH" ]]; then
+  (
+    brew reinstall antigen &&
+    curl -L git.io/antigen > "$ANTIGEN_SCRIPT_PATH" &&
+    chmod +x "$ANTIGEN_SCRIPT_PATH"
+  )
+  reload_zsh
+  fi
+  zsh -i -c "antigen cleanup"
+  zsh -i -c "antigen update"
+}
+
+function _lunarvim {
+  task "Install and update LunarVim"
+  if command -v lvim; then
+    lvim +LvimUpdate +q
+    lvim --headless "+Lazy! sync" +qa
+  else
+    bash <(curl -s $LVIM_INSTALL_SCRIPT) $EXTRA_ARGS || exit 1
+
+    # NOTE: Backup the config folder before removing it and symlink
+    CURRENT_TIMESTAMP="$(date +%s)"
+    /bin/cp -rf "$CONFIG_LVIM_DIR" "$CONFIG_LVIM_DIR.backup-$CURRENT_TIMESTAMP"
+    rm -rf "$CONFIG_LVIM_DIR"
+    create_symlink "$DOTFILES_DIR/lvim" "$CONFIG_LVIM_DIR"
+
+    TEMP_DIR=$(mktemp -d)
+    for L in "${NVIM_SPELL_LANGUAGES[@]}"; do
+      set -x
+      debug "Spell check for language (${L}) is missing, downloading it..."
+      wget -N -nv "ftp://ftp.vim.org/pub/vim/runtime/spell/$L.*" --timeout=5 -P "$TEMP_DIR" || exit 1
+      set +x
+    done
+    for SD in "${NVIM_SPELL_DIRS[@]}"; do
+      [[ -d "$SD" ]] || mkdir -p "$SD"
+      # NOTE: default aliased to `cp -i`
+      /bin/cp -rf "$TEMP_DIR" "$SD/"
+    done
+  fi
+}
+
+function _kubernetes_plugins {
+  task "Install and update kubernetes plugins with krew"
+  KREW_INSTALLED=$(kubectl krew version); echo "$?"
+  if [[ ! $KREW_INSTALLED ]]; then
+  (
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew
+  )
+  fi
+  K8S_PLUGINS_INSTALLED=$(kubectl krew list | tail -n +2 | sort)
+  for PLG in "${KUBERNETES_PLUGINS[@]}"; do
+    if [[ "$K8S_PLUGINS_INSTALLED" != *"$PLG"* ]]; then
+      set -x
+      debug "Installing $PLG since it's missing on system"
+      kubectl krew install "$PLG"
+      continue
+      set +x
+    fi
+    kubectl krew upgrade "$PLG"
+  done
+}
+
+# Languages packages
+
+function _python_libs {
+  task "Install python libs"
+  PP="${PYTHON_LIBS[*]}"
+  for P in ${PP}; do
+    pipx install "${P}"
+  done
+
+  for P in "${PYTHON_INJECTIONS[@]}"; do
+    # shellcheck disable=2086
+    pipx inject ${P}
+  done
+
+  pipx upgrade-all -f --include-injected
+
+  info "installing debugpy latest version"
+  if [ ! -f "${HOME}/.debugpy/bin/poetry" ]; then
+    python3 -m venv "${HOME}/.debugpy"
+  fi
+  "${HOME}/.debugpy/bin/pip" install -U pip
+  "${HOME}/.debugpy/bin/pip" install -U git+https://github.com/microsoft/debugpy.git@main
+}
+
+function _golang_libs {
+  task "Install golang libs"
+  for G in "${GO_LIBS[@]}"; do
+    go install "$G"
+  done
+}
+
+function _rust_libs {
+  info "update rust libs"
+  cargo install-update -a
+}
+
+function _node_libs {
+  info "update node libs"
+  npm update -g
+}
+
+function _rtx_reshim {
+  info "reshimming rtx"
+  "${HOME}/.local/share/rtx/bin/rtx" reshim
+}
---
+++ scripts/bootstrapping/macos.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+FUNCTIONS="$DOTFILES_DIR/scripts/utils/functions.sh"
+[[ -s "$FUNCTIONS" ]] && source "$FUNCTIONS"
+
+BOOTSTRAP_COMMONS="$HOME/dotfiles/scripts/bootstrapping/commons.sh"
+[[ -s "$BOOTSTRAP_COMMONS" ]] && source "$BOOTSTRAP_COMMONS"
+
+# Libs
+BREW_PACKAGES=(
+  antigen
+  coreutils
+  curl
+  wget
+  git
+  haproxy
+  htop
+  jenv
+  mercurial
+  neovim
+  nmap
+  zsh
+  gnu-sed # spectre.nvim
+  pipx
+  openssl
+  readline
+  sqlite3
+  xz
+  zlib
+  tcl-tk
+)
+BREW_CASK_PACKAGES=(
+  docker
+  iterm2
+  minishift
+  google-cloud-sdk
+)
+
+function _packages {
+  task "Install and update common brew packages"
+  if ! command -v brew; then
+    curl -ssL -o- https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+    reload_zsh
+  fi
+  brew update
+  for BP in "${BREW_PACKAGES[@]}"; do
+    brew_install_or_update "$BP"
+  done
+  for BCP in "${BREW_CASK_PACKAGES[@]}"; do
+    brew_install_or_update "$BCP" cask
+  done
+  brew autoremove
+}
+
+function _fonts {
+  task "Install nerd fonts"
+  brew tap homebrew/cask-fonts
+  brew_install_or_update font-hack-nerd-font cask
+  # NOTE: change the font on iTerm2 (LunarVim depends on it)
+}
+
+function _ {
+  _symlinks "$@"
+  _packages "$@"
+  _symlinks "$@"
+  _fonts "$@"
+  _jenv  "$@"
+  _rtx "$@"
+  _fonts "$@"
+  _kubernetes_plugins "$@"
+  _zsh "$@"
+  _python_libs "$@"
+  _golang_libs "$@"
+  _node_libs "$@"
+  _rust_libs "$@"
+  _golang_libs "$@"
+  _rust_libs "$@"
+  _lunarvim "$@"
+  _rtx_reshim "$@"
+}
+
+echo
+set -x
+"_${1}" "$@"
